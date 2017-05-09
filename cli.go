a61b82bbb8ab57bf766d52c655b5b1e21f4fed4c
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-05-09 14:30:50 -0400

    Re-add docopt and mount path as argument

--- cli.go
+++ cli.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"github.com/docopt/docopt-go"
 	"log"
 	"os"
 	"time"
 )
 
 func main() {
+	usage := `kraken - EC2/EBS utility
+
+Usage:
+  kraken <mount_path>
+  kraken -h | --help
+  kraken --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.`
+	arguments, _ := docopt.Parse(usage, nil, true, "Kraken 0.1", false)
+
 	currTime := time.Now().UTC()
 	logger := log.New(os.Stderr, "kraken: ", log.Lshortfile)
 	logger.Printf("RUNNING KRAKEN: %s", currTime.Format(time.RFC850))
-	deviceNames, err := AttachEbsVolumes(logger)
-	if err != nil {
-		logger.Println(err)
-		os.Exit(-1)
+	_, ok := arguments["<mount_path>"].(string)
+
+	if ok {
+		deviceNames, err := AttachEbsVolumes(logger)
+		if err != nil {
+			logger.Println(err)
+			os.Exit(-1)
+		}
+		logger.Printf("Attached: %s\n", deviceNames)
 	}
-	logger.Printf("Attached: %s\n", deviceNames)
 }
