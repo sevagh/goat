8acda08a4d736911129b7fd7d67f48f3d636758e
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-05-10 13:01:19 -0400

    Check disk existence with stat before mdadm command

--- diskutil.go
+++ diskutil.go
@@ -7,7 +7,10 @@ import (
 	"os/exec"
 	"strconv"
 	"syscall"
+	"time"
 )
+
+const statAttempts = 5
 
 func MountSingleDrive(driveName string, mountPath string, logger *log.Logger) error {
 	logger.Printf("Mounting single drive: %s", driveName)
@@ -19,6 +22,19 @@ func MountRaidDrives(driveNames []string, mountPath string, raidLevel int, logge
 	if raidLevel != 0 && raidLevel != 1 {
 		return fmt.Errorf("Valid raid levels are 0 and 1")
 	}
+	logger.Printf("Checking if drives exist")
+	for _, driveName := range driveNames {
+		var attempts int
+		for err := fmt.Errorf("dummy_error"); err != nil; err = executeCommand("stat", []string{driveName}, logger) {
+			time.Sleep(time.Duration(1 * time.Second))
+			attempts++
+			if attempts >= statAttempts {
+				logger.Fatalf("Exceeded max (%d) stat attempts waiting for drive %s to exist", statAttempts, driveName)
+				return fmt.Errorf("Stat failed")
+			}
+		}
+	}
+
 	cmd := "mdadm"
 	args := []string{
 		"--create",
