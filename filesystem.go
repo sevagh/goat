242e84fb42e4225a75a3b12a0e22cd749b92c954
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-05-15 16:03:10 -0400

    Add mkdir on mount path

--- filesystem.go
+++ filesystem.go
@@ -12,7 +12,10 @@ func MountSingleVolume(drive EbsVol, logger *log.Logger) error {
 
 
 func MountSingleDrive(driveName string, mountPath string, desiredFs string, logger *log.Logger) error {
-	if err := checkFilesystem(driveName, desiredFs, logger); err != nil {
+	if err := checkAndCreateFilesystem(driveName, desiredFs, logger); err != nil {
+		return err
+	}
+	if err := mkdir(mountPath, logger); err != nil {
 		return err
 	}
 	cmd := "mount"
@@ -20,7 +23,7 @@ func MountSingleDrive(driveName string, mountPath string, desiredFs string, logg
 		driveName,
 		mountPath,
 	}
-	logger.Printf("Executing: %s %s\n", cmd, args)
+	logger.Printf("Executing: %s %s", cmd, args)
 	if _, err := ExecuteCommand(cmd, args, logger); err != nil {
 		logger.Printf("%v", err)
 		return err
@@ -29,7 +32,21 @@ func MountSingleDrive(driveName string, mountPath string, desiredFs string, logg
 	return nil
 }
 
-func checkFilesystem(driveName string, desiredFs string, logger *log.Logger) error {
+func mkdir(mountPath string, logger *log.Logger) error {
+	cmd := "mkdir"
+	args := []string{
+		"-p",
+		mountPath,
+	}
+	logger.Printf("Executing: %s %s", cmd, args)
+	if _, err := ExecuteCommand(cmd, args, logger); err != nil {
+		logger.Printf("%v", err)
+		return err
+	}
+	return nil
+}
+
+func checkAndCreateFilesystem(driveName string, desiredFs string, logger *log.Logger) error {
 	logger.Printf("Checking filesystem on %s", driveName)
         cmd := "blkid"
 	args := []string{
@@ -39,16 +56,27 @@ func checkFilesystem(driveName string, desiredFs string, logger *log.Logger) err
 		"TYPE",
 		driveName,
 	}
-	fsOut, err := ExecuteCommand(cmd, args, logger);
+	fsOut, err := ExecuteCommand(cmd, args, logger)
 	if err != nil {
-		logger.Printf("%v", err)
-		return err
-	}
-	if fsOut.Status == 2 {
-		return nil
+		if fsOut.Status == 2 {
+			logger.Printf("Creating fs %s on %s", desiredFs, driveName)
+			cmd = "mkfs"
+			argsCreateFs := []string{
+				"-t",
+				desiredFs,
+				driveName,
+			}
+			if _, err := ExecuteCommand(cmd, argsCreateFs, logger); err != nil {
+				return err
+			}
+			return nil
+		} else {
+		    logger.Printf("%v", err)
+		    return err
+		}
 	}
 	switch fsOut.Stdout {
-	case desiredFs:
+	case desiredFs+"\n":
 		return nil
 	default:
 		return fmt.Errorf("Desired fs: %s, actual fs: %s", desiredFs, fsOut.Stdout)
--- raid.go
+++ raid.go
@@ -35,18 +35,27 @@ func MountRaidDrives(drives []EbsVol, volId int, logger *log.Logger) error {
 	raidDriveName := "/dev/md" + strconv.Itoa(volId)
 
 	cmd := "mdadm"
-	args := []string{
-		"--create",
+
+	argsExist := []string{
 		raidDriveName,
-		"--level=" + strconv.Itoa(raidLevel),
-		"--name=KRAKEN" + strconv.Itoa(volId),
-		"--raid-devices=" + strconv.Itoa(len(driveNames)),
 	}
-	args = append(args, driveNames...)
-	logger.Printf("Executing: %s %s\n", cmd, args)
-	if _, err := ExecuteCommand(cmd, args, logger); err != nil {
-		logger.Printf("%v", err)
-		return err
+
+	logger.Printf("Checking if %s exists in mdadm", raidDriveName)
+	if _, err := ExecuteCommand(cmd, argsExist, logger); err != nil {
+		logger.Printf("Raid drive doesn't exist, creating")
+		args := []string{
+			"--create",
+			raidDriveName,
+			"--level=" + strconv.Itoa(raidLevel),
+			"--name=KRAKEN" + strconv.Itoa(volId),
+			"--raid-devices=" + strconv.Itoa(len(driveNames)),
+		}
+		args = append(args, driveNames...)
+		logger.Printf("Executing: %s %s\n", cmd, args)
+		if _, err := ExecuteCommand(cmd, args, logger); err != nil {
+			logger.Printf("%v", err)
+			return err
+		}
 	}
 
 	return MountSingleDrive(raidDriveName, mountPath, drives[0].FsType, logger)
