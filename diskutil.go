905a5f89b95ec72a1d1fbdc4a364061e1a2e2aec
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-05-23 10:19:31 -0400

    Persist mdadm settings

--- diskutil.go
+++ diskutil.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"os"
 	log "github.com/sirupsen/logrus"
+	"os"
 )
 
 func PrepAndMountDrives(volName string, vols []EbsVol, dryRun bool) {
--- raid.go
+++ raid.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	log "github.com/sirupsen/logrus"
+	"os"
 	"strconv"
 )
 
@@ -37,10 +38,39 @@ func CreateRaidArray(drives []EbsVol, volName string, dryRun bool) string {
 		"--raid-devices=" + strconv.Itoa(len(driveNames)),
 	}
 	args = append(args, driveNames...)
-	log.Info("Creating RAID drive: %s %s", cmd, args)
+	raidLogger.Info("Creating RAID drive: %s %s", cmd, args)
 	if _, err := ExecuteCommand(cmd, args); err != nil {
 		raidLogger.Fatalf("Error when executing mdadm command: %v", err)
 	}
 
+	args = []string{
+		"--verbose",
+		"--detail",
+		"--scan",
+	}
+
+	raidLogger.Info("Persisting mdadm settings: %s %s", cmd, args)
+	if out, err := ExecuteCommand(cmd, args); err != nil {
+		raidLogger.Fatalf("Error when executing mdadm command: %v", err)
+	} else {
+		if err := appendToMdadmConf(out.Stdout); err != nil {
+			raidLogger.Fatalf("Error when persisting mdadm settings to /etc/mdadm.conf: %v", err)
+		}
+	}
+
 	return raidDriveName
+}
+
+func appendToMdadmConf(content string) error {
+	f, err := os.OpenFile("/etc/mdadm.conf", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	if _, err = f.WriteString(content); err != nil {
+		return err
+	}
+	return nil
 }
