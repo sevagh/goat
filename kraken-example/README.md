8a7fd48cdefc2f1db5e84a98f78c960b001f4253
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-05-28 05:50:42 -0400

    Move kraken-example to branch example

--- kraken-example/README.md
+++
@@ -1,3 +0,0 @@
-# kraken-example
-
-Terraform recipe for a minimally viable example of my [Kraken](https://github.com/sevagh/kraken) EBS/EC2 tag-based mounting system.
--- kraken-example/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region                  = "us-east-1"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  profile                 = "default"
-}
