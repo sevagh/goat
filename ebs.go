2dc63a4eaeb29d703c2bed64c0ef23a36162c1e0
Rob Greenwood <bilco105@gmail.com>
2019-08-13 11:42:26 +0100

    Fixed failure to mount volumes
    For some reason, syscall.Mount is no longer able to identify the filesystem on some (nvme) volumes, so fails to mount.
    
    Fixed by shelling out directly to the mount command for now.

--- ebs.go
+++ ebs.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	log "github.com/sirupsen/logrus"
 	"os"
+	"os/exec"
 	"strconv"
-	"syscall"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
@@ -120,7 +120,8 @@ func prepAndMountDrives(volName string, vols []EbsVol) {
 	}
 
 	driveLogger.Info("Now mounting")
-	if err := syscall.Mount("", mountPath, "", 0, ""); err != nil {
+	cmd := exec.Command("mount", mountPath)
+	if err := cmd.Run(); err != nil {
 		driveLogger.Fatalf("Couldn't mount: %v", err)
 	}
 
